b69987531951715d1962856f80cfff58099077ff
divyachoudhary876-rgb <divyachoudhary876@gmail.com>
2026-09-12 14:54:35 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // ⚠️ Yeh line Electron / Desktop app ke liye sabse zyada zaroori hai!
   server: {
     port: 3000,
     proxy: {
